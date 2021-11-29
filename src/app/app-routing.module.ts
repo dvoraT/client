09fd15b307ecefff635aa6bfaae6b2383f091886
dvoraT <devoratebeka@gmail.com>
2021-11-29 11:29:21 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { NavComponent } from './nav/nav.component';
+import { NewslatterComponent } from './newslatter/newslatter.component';
+import { RecommendationsComponent } from './recommendations/recommendations.component';
+import { StatisticComponent } from './statistic/statistic.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "Nav", component:NavComponent, children:[
+  {path:"Home", component:HomeComponent},
+  {path:"About", component:AboutComponent},
+  {path:"Recommendations", component:RecommendationsComponent},
+  {path:"Statistic", component:StatisticComponent},
+  {path:"NewsLatter", component:NewslatterComponent},
+  {path:"Contact", component:ContactComponent}
+    ]},
+    
+ 
+  
+  
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,37 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AboutComponent } from './about/about.component';
+import { RecommendationsComponent } from './recommendations/recommendations.component';
+import { StatisticComponent } from './statistic/statistic.component';
+import { NewslatterComponent } from './newslatter/newslatter.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    
+    AboutComponent,
+    
+    RecommendationsComponent,
+    
+    StatisticComponent,
+    
+    NewslatterComponent,
+    
+    ContactComponent,
+    
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    NoopAnimationsModule,
     
   ],
   providers: [],
